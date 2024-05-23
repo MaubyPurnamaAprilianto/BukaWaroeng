41ebe56a7e8eccb9bdfd0119bbb3c59a4cdeb681
maubypurnama <maubypurnama010407@gmail.com>
2024-05-23 12:31:13 +0700

    [fix]fitur create

--- logikatambahproduk.php
+++ logikatambahproduk.php
@@ -1,20 +1,37 @@
 <?php
+
 session_start();
 $conn = mysqli_connect("localhost", "root", "", "db_katalog_produk");
 
 $name = $_POST['nama'];
 $harga = $_POST['harga'];
 $decs = $_POST['deskripsi'];
-$image = $_FILES['gambar']['name'];
 
-$query = "INSERT INTO produk ( produk_nama, produk_harga, produk_deksripsi, produk_image) VALUES ( '$name', '$harga', '$decs', '$image')";
-$result = mysqli_query($conn, $query);
+// menampung data file yang diupload
+$filename = $_FILES['gambar']['name'];
+$tmp_name = $_FILES['gambar']['tmp_name'];
 
-if ($result) {
-  move_uploaded_file($_FILES['image']['tmp_name'], 'produk/' . $image);
-  header('Location: tambah_produk.php');
+$type1 = explode('.', $filename);
+$type2 = $type1[1];
+
+$newname = 'produk'.time().'.'.$type2;
+
+// menampung data format file yang diizinkan
+$allowtype = array('jpg', 'jpeg', 'png', 'gif');
+
+if (!in_array($type2, $allowtype)) {
+  echo "Type file tidak diizinkan";
 } else {
-  echo "Failed";
+  move_uploaded_file($tmp_name, 'produk/' . $newname);
+
+  $query = "INSERT INTO produk ( produk_nama, produk_harga, produk_deksripsi, produk_image) VALUES ( '$name', '$harga', '$decs', '$newname')";
+  $result = mysqli_query($conn, $query);
+  if ($result) {
+      move_uploaded_file($_FILES['image']['tmp_name'], 'produk/' . $image);
+      header('Location: data_produk.php');
+  } else {
+      echo "Failed";
+  }
 }
 
-?>
+
